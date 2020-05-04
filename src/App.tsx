14d4d3029ec5806dc768c92f5db17c25c5d40444
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-05-04 21:34:58 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { Navbar } from './components/Navbar';
+import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+
+
+
+const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useState([])
+
+  const addHandler = (title: string) => {
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    setTodos([newTodo, ...todos])
+  }
+
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <TodoForm onAdd={addHandler} />
+
+        <TodoList todos={todos} />
+      </div>
+    </>
+  )
+}
+
+export default App;
